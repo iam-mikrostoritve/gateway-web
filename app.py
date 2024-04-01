01a0f5fccda18ed02b024dcb0bd74898a65a1020
PolnerFilip <filip.polner@student.um.si>
2024-04-01 18:06:32 +0200

    implemented user service gateway

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask
+import os
+
+import requests
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_cors import CORS
+
+load_dotenv()
 
 app = Flask(__name__)
+cors = CORS(app)
+
+USER_SERVICE_URL = os.getenv('USER_SERVICE_URL')
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
 
+@app.route('/', methods=['GET'])
+def health_check():
+    return {'Status': 'Running'}, 200
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    if not body:
+        return {'error': 'Request body is required'}, 400
+    if 'email' not in body or 'password' not in body:
+        return {'error': 'Email and password are required'}, 400
+
+    response = requests.post(f'{USER_SERVICE_URL}/login', json=body)
+    return response.json(), response.status_code
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    body = request.get_json()
+    if not body:
+        return {'error': 'Request body is required'}, 400
+    if 'email' not in body or 'password' not in body:
+        return {'error': 'Email and password are required'}, 400
+
+    if 'first_name' not in body:
+        body['first_name'] = ''
+    if 'last_name' not in body:
+        body['last_name'] = ''
+    body['role'] = 'USER'
+
+    response = requests.post(f'{USER_SERVICE_URL}/signUp', json=body)
+    return response.json(), response.status_code
+
+
+@app.route('/api/', methods=['GET'])
+def get_artists():
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
